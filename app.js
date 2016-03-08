77615a5c3fd3436a3e5f592356a6b89d48dd8fab
David R. Jones <drj@rackfx.com>
2016-03-07 18:45:20 -0700

    added stuff

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 console.log('hello world');
 
+function sayHello(word){
+  console.log("hello "+ word);
+}
+
+sayHello("David");
 
 // Method 1
 var o = { };
@@ -7,17 +12,39 @@ o.name = "David";
 o.address = "123 Main Street";
 o.age = 19;
 o.age += 5;
+o.sayHello = function(){
+  console.log("Hello object method");
+}
+
+o.sayHello();
+
+o.sayHello2 = sayHello;
+
 
 // Method 2
 var o2 = {
   name: "David",
   address: "123 Main Street",
-  age: 19 +5
+  age: 19 +5,
+  sayHello: function(){
+    console.log("hello, object method!!");
+
+  }
 };
+
+o2.sayHello();
 
 o2.age +=5;
 
 o2.name = "Mr. " + o2.name + " jones";
+
+var myArray = ["Apples", "Oranges", "Kiwi"]
+
+
+console.log(myArray);
+
+
+
 
 
 console.log(o);
