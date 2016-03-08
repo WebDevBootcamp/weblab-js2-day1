4b6a7b6e3799cf24c5efcf6ed7182b1110fd4296
David R. Jones <drj@rackfx.com>
2016-03-07 18:31:02 -0700

    added stuff

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ var o = { };
 o.name = "David";
 o.address = "123 Main Street";
 o.age = 19;
-
-o.age ++ 5;
-
-
+o.age += 5;
 
 // Method 2
 var o2 = {
   name: "David",
-  address: "123 Main Street"
-}
+  address: "123 Main Street",
+  age: 19 +5
+};
+
+o2.age +=5;
+
+o2.name = "Mr. " + o2.name + " jones";
 
 
 console.log(o);
