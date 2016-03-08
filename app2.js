b14a59f7983fa49592ec509a7da8b916812e6859
David R. Jones <drj@rackfx.com>
2016-03-07 19:39:07 -0700

    added stuff

--- app2.js
+++ app2.js
@@ -1,1 +1,36 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+
 console.log('hi');
+
+
+var properties = [
+  {
+    ownerName: "David Jones",
+    address: "123 Main Street"
+  },
+  {
+    ownerName: "Bob Johnson",
+    address: "345 Einsenhower Blvd"
+  },
+  {
+    ownerName: "Mary Smith",
+    address: "678 Spruce St."
+  }
+];
+
+
+console.log(properties);
