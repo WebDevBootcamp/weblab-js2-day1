fa14bec2dbb9e3e520a92257f26dc431588fd5ec
David R. Jones <drj@rackfx.com>
2016-03-07 18:27:05 -0700

    made changes

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log('hello world');
+
+
+// Method 1
+var o = { };
+o.name = "David";
+o.address = "123 Main Street";
+o.age = 19;
+
+o.age ++ 5;
+
+
+
+// Method 2
+var o2 = {
+  name: "David",
+  address: "123 Main Street"
+}
+
+
+console.log(o);
+console.log(o2);
